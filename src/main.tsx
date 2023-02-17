ff1f9e89064ba4bb9e12f7a79ddc8f10533dc93a
ThomasHardy98 <tj.hardy77@yahoo.co.uk>
2023-02-17 11:36:24 +0000

    Added strict mode back

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "App";
@@ -7,7 +8,9 @@ import "main.scss";
 
 // Wrap location provider around app as most elements need access to it's context
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <LocationProvider>
-    <App />
-  </LocationProvider>
+  <React.StrictMode>
+    <LocationProvider>
+      <App />
+    </LocationProvider>
+  </React.StrictMode>
 );
